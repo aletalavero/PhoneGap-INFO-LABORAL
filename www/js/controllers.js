2f1a64afc60363bdf3673e52292ee4b79019261e
aletalavero <ptalavero76@iesnumancia.cat>
2016-01-14 12:01:23 +0100

    Update controllers.js
    Actualitzats els planetas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,10 +9,32 @@ $JSView.controller = {
     sol: function(e){
 		$JSView.dataView({},e)	
 	},
+    mercuri: function(e){
+		$JSView.dataView({},e)
+	},
     venus: function(e){
+		$JSView.dataView({},e)
+	},
+    terra: function(e){
+		$JSView.dataView({},e)
+	},
+    mart: function(e){
+		$JSView.dataView({},e)
+	},
+    jupiter: function(e){
+		$JSView.dataView({},e)
+	},
+    ura: function(e){
+		$JSView.dataView({},e)
+	},
+    neptu: function(e){
+		$JSView.dataView({},e)
+	},
+    saturn: function(e){
 		$JSView.dataView({},e)
 	},
     modalA: function(e){
 		$JSView.dataView({},e)	
 	}
 }
+
